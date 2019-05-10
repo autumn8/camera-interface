805dd80c3c4d0bfdf6b2703d99b151da19b41dd3
Brad Roodt <brad.roodt@gmail.com>
2019-05-10 08:20:29 +0200

    feat: added placeholder image and camera routing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-layout justify-center>
     <v-flex xs12 sm6>
       <v-card>
-        <v-container fluid grid-list-md>
+        <v-container fluid grid-list-md>            
           <v-layout row wrap>
             <v-flex
               v-for="(card, index) in cards"
@@ -10,11 +10,11 @@
               v-bind="{ [`xs${card.flex}`]: true }"
             >
               <v-card>
-                <v-img :src="card.src" height="300px" aspect-ratio="0.25">                  
+                <v-img :src="frame" height="300px" aspect-ratio="0.25">
                 </v-img>
 
                 <v-card-actions>
-                    <span class="camera-name">CAMERA {{index}}</span>
+                  <span class="camera-name">CAMERA {{ index }}</span>
                   <v-spacer></v-spacer>
                   <v-btn icon>
                     <v-icon>portrait</v-icon>
@@ -22,7 +22,7 @@
                   <v-btn icon>
                     <v-icon>crop_free</v-icon>
                   </v-btn>
-                  <v-btn icon :to = "'/camera/' + index">
+                  <v-btn icon :to="'/camera/' + index">
                     <v-icon>edit</v-icon>
                   </v-btn>
                 </v-card-actions>
@@ -36,27 +36,29 @@
 </template>
 
 <script>
-export default {
+
+
+export default {      
   data() {
     return {
+      frame: require('@/assets/image-placeholder.png'),        
       cards: [
         {
           title: "Pre-fab homes",
           src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
           flex: 12
         }
-        
       ]
     };
-  }
+  },
+   mounted() {        
+    },
 };
 </script>
 
 <style>
-    .camera-name {
-        font-size: 1rem;
-        font-weight: 700;
-    }
+.camera-name {
+  font-size: 1rem;
+  font-weight: 700;
+}
 </style>
-
-
