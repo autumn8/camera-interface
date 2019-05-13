e7bc2bbbe93ffdfb7697ee0d016190f36abd31f1
Brad Roodt <brad.roodt@gmail.com>
2019-05-13 21:48:31 +0200

    feat: beginnings of frame handling in mqtt

--- src/mqtt.js
+++ src/mqtt.js
@@ -3,12 +3,11 @@ import store from "@/store";
 
 const hostname = "mqtt://localhost:9001";
 const client = mqtt.connect(hostname);
-//const cameraNameLookup;
 
 function init() {
-  client.on("connect", function() {
+  client.on("connect", function () {
     console.log("connected");
-    client.subscribe("camera/connected/#");    
+    client.subscribe("camera/connected/#");
     //client.subscribe("camera/image/inference");
     client.subscribe("camera/frame/camera1");
   });
@@ -17,23 +16,18 @@ function init() {
     console.log("topic", topic);
     if (topic.includes("camera/connected")) {
       const routeSegments = topic.split("/");
-      const camera = routeSegments[routeSegments.length - 1];
+      const cameraName = routeSegments[routeSegments.length - 1];
       const payload = message.toString();
-      if (payload == 1) {  
-        //TODO add checks to ensure camera isn't readded if it disconnects and reconnects.        
-        store.commit('addCamera', camera); 
-        client.subscribe(`camera/frame/${camera.name}`);
+      if (payload == 1) {
+        store.commit("addCamera", cameraName);
+        client.subscribe(`camera/frame/${cameraName}`);
         return;
       }
-      /* if (topic.includes("camera/frame")) {
-        const routeSegments = topic.split("/");
-        const cameraName = routeSegments[routeSegments.length - 1];
-        const payload = message.toString();
-      } */
-
-
-      console.log(camera);
-      console.log(payload);
+    }
+    if (topic.includes("camera/frame")) {
+      const routeSegments = topic.split("/");
+      const cameraName = routeSegments[routeSegments.length - 1];
+      const frame = `data:image/jpeg;base64, ${message.toString("base64")}`;
     }
   });
 }
