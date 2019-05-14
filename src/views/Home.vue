88865371a0d1dc14137a80d7ac68bbfd9f696a85
Brad Roodt <brad.roodt@gmail.com>
2019-05-14 07:35:25 +0200

    feat: added no camera text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,9 @@
       <v-container fluid grid-list-md>
         <v-layout row wrap>
           <v-flex v-for="(camera, index) in cameras" :key="index">
-            <compact-camera :index="index"></compact-camera>
+            <compact-camera :index="index"></compact-camera>            
           </v-flex>
+          <div class="text-xs-center no-cameras" v-if="!cameras.length">No cameras found</div>
         </v-layout>
       </v-container>
     </v-flex>
@@ -31,5 +32,9 @@ export default {
 .camera-name {
   font-size: 1rem;
   font-weight: 700;
+}
+
+.no-cameras {
+  width:100%;
 }
 </style>
