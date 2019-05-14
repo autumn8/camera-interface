df9d1a08b82048c9dbc82e57df8410fc2c15b8a2
Brad Roodt <brad.roodt@gmail.com>
2019-05-14 07:36:18 +0200

    feat: renamed frame to currentFrame

--- src/components/CompactCamera.vue
+++ src/components/CompactCamera.vue
@@ -1,18 +1,22 @@
 <template>
   <v-card>
-    <v-img :src="frame" height="300px" aspect-ratio="0.25"></v-img>
+    <v-img :src="currentFrame || placeHolder" height="300px" aspect-ratio="0.25"></v-img>
 
     <v-card-actions>
       <span class="camera-name"> {{ camera.name }}</span>
       <v-spacer></v-spacer>
-      <v-btn v-model="camera.isDetectionEnabled" icon @click="toggleIsDetectionEnabled">
+      <v-btn
+        v-model="camera.isDetectionEnabled"
+        icon
+        @click="toggleIsDetectionEnabled"
+      >
         <v-icon :color="detectionEnabledClass">portrait</v-icon>
       </v-btn>
 
       <!--<v-btn :disabled="!detectionEnabled" v-model="detectionZoneEnabled" icon @click="detectionZoneEnabled = !detectionZoneEnabled">
         <v-icon :color="detectionZoneEnabledClass">crop_free</v-icon>
       </v-btn>-->
-      
+
       <v-btn icon :to="`/camera/${index}`">
         <v-icon>edit</v-icon>
       </v-btn>
@@ -23,34 +27,35 @@
 <script>
 
 export default {
-  props: ['index'],
-  computed: {    
+  props: ["index"],
+  computed: {
     detectionEnabledClass() {
-      return this.camera.isDetectionEnabled ? 'green' : 'dark-grey'
+      return this.camera.isDetectionEnabled ? "green" : "dark-grey";
     },
     detectionZoneEnabledClass() {
-      return this.camera.isDetectionZoneEnabled ? 'green' : 'dark-grey'
+      return this.camera.isDetectionZoneEnabled ? "green" : "dark-grey";
     },
     camera() {
-      return this.$store.state.cameras[this.index]
+      return this.$store.state.cameras[this.index];
     }
   },
-  methods:{
+  methods: {
     toggleIsDetectionEnabled() {
       this.camera.isDetectionEnabled = !this.camera.isDetectionEnabled;
     }
   },
   data() {
-    return {      
-      frame: require('@/assets/image-placeholder.png')  
-    }
+    return {
+      currentFrame: null,
+      placeHolder: require("@/assets/image-placeholder.png")
+    };
   }
 };
 </script>
 
 <style>
 .v-icon.green {
-  color:green;
+  color: green;
 }
 
 .v-icon.red {
--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -6,65 +6,58 @@
           <v-flex>
             <v-card>
               <div class="camera-container">
-                  <v-img :src="frame" height="300px" aspect-ratio="0.25"></v-img>
-                  <canvas
-                    class="camera__canvas"
-                    id="camera__canvas"
-                    ref="camera1DetectionZone"
-                    width="300px"
-                    height="300px"
-                  ></canvas>
+                <v-img :src="currentFrame || placeHolder" height="300px" aspect-ratio="0.25"></v-img>
+                <canvas
+                  class="camera__canvas"
+                  id="camera__canvas"
+                  ref="camera1DetectionZone"
+                  width="300px"
+                  height="300px"
+                ></canvas>
               </div>
-              
 
               <v-card-actions>
-                <span class="camera-name">{{ camera.name }}</span>
+                <span class="camera-name">{{camera.name}}</span>
                 <v-spacer></v-spacer>
               </v-card-actions>
 
               <v-container>
                 <v-layout>
                   <v-switch
-                    v-model="camera.isDetectionEnabled"                    
-                    :label="`Intruder detection is ${camera.isDetectionEnabled ? 'on' : 'off'}`"
+                    v-model="camera.isDetectionEnabled"
+                    :label="
+                      `Intruder detection is ${
+                        camera.isDetectionEnabled ? 'on' : 'off'
+                      }`
+                    "
                   ></v-switch>
                   <div class="info-icon-container">
-                      <v-btn icon @click="showDetectionZoneInfo">
-                    <v-icon class="stuff" color="teal">info</v-icon>
-                  </v-btn>
+                    <v-btn icon @click="showDetectionZoneInfo">
+                      <v-icon class="stuff" color="teal">info</v-icon>
+                    </v-btn>
                   </div>
-                  
                 </v-layout>
 
-                <v-layout >
+                <v-layout>
                   <v-switch
-                    :disabled="!camera.isDetectionEnabled" :class="{'half-opacity' : !camera.isDetectionEnabled}"
+                    :disabled="!camera.isDetectionEnabled"
+                    :class="{ 'half-opacity': !camera.isDetectionEnabled }"
                     v-model="camera.isZoneEnabled"
                     :label="`Zones are ${camera.isZoneEnabled ? 'on' : 'off'}`"
                   ></v-switch>
                   <v-btn icon @click="showDetectionZoneInfo">
                     <v-icon class="stuff" color="teal">info</v-icon>
                   </v-btn>
+
+                  <v-flex xs12>
+                    <v-slider v-model="slider" label="Label"></v-slider>
+                  </v-flex>
                 </v-layout>
                 <!-- <div class="text-xs-center">
                 <v-btn color="success" @click="saveSettings">Save</v-btn>
-              </div> -->
+                </div>-->
               </v-container>
-
               
-
-              <!-- <div>
-                
-                <div class="camera1">
-                  <canvas
-                    class="camera1__canvas"
-                    id="camera1__canvas"
-                    ref="camera1DetectionZone"
-                    width="300px"
-                    height="300px"
-                  ></canvas>
-                </div>
-              </div>-->
             </v-card>
           </v-flex>
         </v-layout>
@@ -74,77 +67,43 @@
 </template>
 
 <script>
-
 //$route.params.id
 export default {
-    computed: {
-        camera() {
-            return this.$store.state.cameras[this.$route.params.index]
-        }        
-    },
   data() {
-    return {      
-      frame: require("@/assets/image-placeholder.png")
-    };
+    return {
+      currentFrame: null,
+      placeHolder: require("@/assets/image-placeholder.png")
+    }
+  },
+  computed: {
+    camera() {
+      return this.$store.state.cameras[this.$route.params.index];
+    }
   },
   methods: {
     showDetectionZoneInfo() {}
   },
-  mounted() {
-    /* const canvas = this.$refs.camera1DetectionZone;
-    const context = this.$refs.camera1DetectionZone.getContext("2d");
-    let startX = null;
-    let startY = null;
-    const onPointerDown = e => {
-        console.log(e.touches[0]);
-      const event = e.touches[0];
-      startX = event.clientX;
-      startY = event.clientY;
-    };
-
-    const onPointerUp = e => {
-      console.log("pointer up");
-    };
-
-    const onPointerMove = e => {
-      e.preventDefault();
-      context.clearRect(0, 0, 300, 300);
-      context.globalAlpha = 0.5;
-      
-      const endX = e.touches[0].clientX - startX - event.target.offsetLeft;
-      const endY = e.touches[0].clientY - startY - event.target.offsetTop;
-      context.fillRect(startX, startY, endX, endY);
-      context.globalAlpha = 1;
-      return false;
-    };
-
-    canvas.addEventListener("touchstart", onPointerDown, false);
-    canvas.addEventListener("touchend", onPointerUp, false);
-
-    canvas.addEventListener("touchmove", onPointerMove, false); */
-  }
+  mounted() {}
 };
 </script>
 
-
 <style>
-.info-icon-container{
-    margin-top:8px;
+.info-icon-container {
+  margin-top: 8px;
 }
 
 .half-opacity {
-    opacity: 0.5;
+  opacity: 0.5;
 }
 
 .camera-container {
-    position:relative;
+  position: relative;
 }
 
-.camera__canvas{
-    scroll:none;
-    position: absolute;
-    top:0;
-    left:0;
+.camera__canvas {
+  scroll: none;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
-
