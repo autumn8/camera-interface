94a654cd0438fd1abc7b275a2f6b49290718f752
Brad Roodt <brad.roodt@gmail.com>
2019-05-21 22:33:59 +0200

    feat: cleaned up mqtt subs

--- src/mqtt.js
+++ src/mqtt.js
@@ -6,26 +6,31 @@ const host = `${mqttHostAddress}:${mqttHostPort}`;
 const client = mqtt.connect(host);
 client.on("connect", function() {
   console.log("connected");
-  client.subscribe("camera/connected/#");
-  client.subscribe("camera/settingsupdate/#");
+  client.subscribe("camera/connected/#");  
 });
 client.on("message", (topic, message) => {
   const payload = message.toString();
   console.log("topic", topic);
-  if (topic.includes("camera/settingsupdate")) {
+  /* if (topic.includes("camera/settingsupdate")) {
     console.log("initialized message");
     console.log(JSON.parse(payload));
-    store.commit("addCamera", JSON.parse(payload));
-  }
+    
+  } */
   if (topic.includes("camera/connected")) {
     const routeSegments = topic.split("/");
     const cameraName = routeSegments[routeSegments.length - 1];
-    if (payload == 1) {
-
-      client.subscribe(`camera/frame/${cameraName}`);
-      return;
+    if (payload === "True") {
+      client.subscribe(`camera/settingsupdate/${cameraName}`);
+    } else {
+      eventBus.$emit(`camera/frame/${cameraName}`, null);
     }
-    if (payload == 0) eventBus.$emit(`camera/frame/${cameraName}`, null);
+  }
+  if (topic.includes("camera/settingsupdate")){
+    const cameraSettings = JSON.parse(payload);
+    const routeSegments = topic.split("/");
+    const cameraName = routeSegments[routeSegments.length - 1];
+    store.commit("addCamera", cameraSettings);
+    client.subscribe(`camera/frame/${cameraName}`);
   }
   if (topic.includes("camera/frame")) {
     const routeSegments = topic.split("/");
